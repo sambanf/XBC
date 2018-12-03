fc971b12145361a6fed6cd7c0f68a672632e34e9
sambanf <sambanugraha@gmail.com>
2018-12-03 09:27:01 +0700

    Add project files.

---
+++ MinPro180.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MinPro180.MVC.MvcApplication" Language="C#" %>
---
+++ MinPro180.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MinPro180.MVC.Startup))]
+namespace MinPro180.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
