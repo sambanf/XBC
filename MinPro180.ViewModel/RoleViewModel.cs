31e5774d9acf6305823b54b782001bce44bc6170
mochammadsobandi197 <mochammad.sobandi@outlook.co.id>
2018-12-05 13:41:24 +0700

    add menu create

--- MinPro180.ViewModel/RoleViewModel.cs
+++ MinPro180.ViewModel/RoleViewModel.cs
@@ -15,18 +15,22 @@ namespace MinPro180.ViewModel
         }
         public long id { get; set; }
 
+        [Display(Name = "CODE")]
         [Required]
         [StringLength(50)]
         public string code { get; set; }
 
+        [Display(Name = "NAME")]
         [Required]
         [StringLength(50)]
         public string name { get; set; }
 
+        [Display(Name = "DESCRIPTION")]
         [Required]
         [StringLength(255)]
         public string description { get; set; }
 
+        [Display(Name = "STATUS")]
         public bool active { get; set; }
     }
 }
