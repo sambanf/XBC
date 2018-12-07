88197093989f7d505ebc3b2c8399e1092c7148e6
mochammadsobandi197 <mochammad.sobandi@outlook.co.id>
2018-12-07 16:19:55 +0700

    login

---
+++ MinPro180.MVC/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MinPro180.ViewModel;
+using MinPro180.DataModel;
+using MinPro180.Repository;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+
+namespace MinPro180.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModelcs objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MinProContext db = new MinProContext())
+                {
+                    var obj = db.t_user.Where(a => a.username.Equals(objUser.username) && a.password.Equals(objUser.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["id"] = obj.id.ToString();
+                        Session["username"] = obj.username.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["id"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+    }
+}
---
+++ MinPro180.ViewModel/LoginViewModelcs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinPro180.ViewModel
+{
+    public class LoginViewModelcs
+    {
+        public long id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "USERNAME")]
+        public string username { get; set; }
+
+        [Required]
+        [Display(Name = "PASSWORD")]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
